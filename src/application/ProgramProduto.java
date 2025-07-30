44e23e4ee390d71a1b2dd17584889c7aceebe762
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-30 01:23:18 -0300

    adiciona leitura dos dados do produto e inicia com o construtor correto

--- src/application/ProgramProduto.java
+++ src/application/ProgramProduto.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Produto;
+
 public class ProgramProduto {
 
 	public static void main(String[] args) {
@@ -12,6 +14,26 @@ public class ProgramProduto {
 
 		System.out.println("Iniciando o programa...");
 		System.out.println("-----------------------");
+
+		Produto produtoA;
+
+		System.out.println("Dados do produto:");
+		System.out.print("Código: ");
+		int codigo = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Deseja cadastrar a quantidade e o preço do produto?(s/n) ");
+		char resposta = sc.next().charAt(0);
+		if (resposta == 's') {
+			System.out.print("Quantidade: ");
+			int quantidade = sc.nextInt();
+			System.out.print("Preço: ");
+			double preco = sc.nextDouble();
+			produtoA = new Produto(codigo, nome, quantidade, preco);
+		}else {
+			produtoA = new Produto(codigo, nome);
+		}
 
 		sc.close();
 

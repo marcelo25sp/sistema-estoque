db0c8ff8fa81c0e72e1fb7ac94e104960a9fccb2
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-30 01:11:43 -0300

    cria estrutura básica do programa com Scanner e Locale

--- src/application/ProgramProduto.java
+++ src/application/ProgramProduto.java
@@ -1,9 +1,19 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramProduto {
 
 	public static void main(String[] args) {
-		
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Iniciando o programa...");
+		System.out.println("-----------------------");
+
+		sc.close();
 
 	}
 

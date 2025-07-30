ea1c5eec2074c252807838e37a6656db19210b17
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-30 00:58:50 -0300

    adiciona dois construtores ao programa Produto.java

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -6,5 +6,17 @@ public class Produto {
 	private String nome;
 	private int quantidade;
 	private double preco;
+	
+	public Produto(int codigo, String nome, int quantidade, double preco) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	public Produto(int codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
 
 }

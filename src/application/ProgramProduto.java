3c6d678ec0097a018ed06213e515e83c333924aa
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2025-07-31 00:13:47 -0300

    Adiciona comentários ao programa

--- src/application/ProgramProduto.java
+++ src/application/ProgramProduto.java
@@ -9,18 +9,21 @@ public class ProgramProduto {
 
 	public static void main(String[] args) {
 
+		// Define o padrão de ponto flutuante como o padrão americano (ponto ao invés de vírgula) 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Iniciando o programa...");
 		System.out.println("-----------------------");
 
+		// Declaração da variável do tipo Produto
 		Produto produtoA;
 
+		// Entrada de dados obrigatórios do produto
 		System.out.println("Dados do produto:");
 		System.out.print("Código: ");
 		int codigo = sc.nextInt();
-		sc.nextLine();
+		sc.nextLine(); // consome a quebra de linha pendente (limpeza do buffer)
 		System.out.print("Nome: ");
 		String nome = sc.nextLine();
 		System.out.print("Deseja cadastrar a quantidade e o preço do produto?(s/n) ");
@@ -30,14 +33,18 @@ public class ProgramProduto {
 			int quantidade = sc.nextInt();
 			System.out.print("Preço: ");
 			double preco = sc.nextDouble();
+			// Usa o construtor completo
 			produtoA = new Produto(codigo, nome, quantidade, preco);
 		}else {
+			// Usa o construtor parcial( sem quantidade e preço)
 			produtoA = new Produto(codigo, nome);
 		}
 		
+		// Exibe os dados atuais do produto
 		System.out.println();
 		System.out.println(produtoA);
 		
+		// Entrada para adicionar quantidade
 		System.out.println();
 		System.out.print("Entre com a quantidade a ser adicionada? ");
 		int quantidade = sc.nextInt();
@@ -45,6 +52,7 @@ public class ProgramProduto {
 		System.out.println("---------------------------------------");
 		System.out.println(produtoA);
 		
+		// Entrada para remover quantidade
 		System.out.println();
 		System.out.print("Entre com a quantidade a ser removida: ");
 		quantidade = sc.nextInt();
--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,5 +1,10 @@
 package entities;
 
+/*
+ * Classe Produto representa um item com código, nome, quantidade e preço
+ * Fornece métodos para adicionar e remover a quantidade do estoque
+ */
+
 public class Produto {
 	
 	private int codigo;
@@ -7,6 +12,7 @@ public class Produto {
 	private int quantidade;
 	private double preco;
 	
+	// Construtor completo
 	public Produto(int codigo, String nome, int quantidade, double preco) {
 		this.codigo = codigo;
 		this.nome = nome;
@@ -14,21 +20,25 @@ public class Produto {
 		this.preco = preco;
 	}
 	
+	// Construtor com apenas código e nome
 	public Produto(int codigo, String nome) {
 		this.codigo = codigo;
 		this.nome = nome;
 	}
 	
+	// Adiciona uma quantidade ao estoque
 	public void adicionarEstoque(int quantidade) {
 		this.quantidade += quantidade;
 	}
 	
+	// Remove uma quantidade ao estoque (se houver quantidade suficiente)
 	public void removerEstoque(int quantidade) {
 		if(quantidade <= this.quantidade) {
 			this.quantidade -= quantidade;
 		}		
 	}
 	
+	// Retorna uma string formatada com os dados do produto
 	public String toString() {
 		return "Dados do produto:\n"
 			 + "Código: " + codigo+ "\n"
